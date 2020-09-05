64b9a029b62ce36b1b07e0e0268f9f7ff7502310
Panithi Nakkhruea <panithi.nakkhruea@protonmail.com>
2020-09-05 18:18:59 +0700

    created custom user model.

--- surus/settings.py
+++ surus/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'apps.user'
 ]
 
 MIDDLEWARE = [
@@ -128,3 +129,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+#Custom user model
+AUTH_USER_MODEL = 'user.User'
